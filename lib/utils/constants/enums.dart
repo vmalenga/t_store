e9e748def19eb731f943d187a9f1d9a6431ac2cd
Adm-vmalenga <Adm-vmalenga@ulitanz.com>
2024-10-24 10:57:31 +0300

    Deleted project files

--- lib/utils/constants/enums.dart
+++
@@ -1,9 +0,0 @@
-
-// LIST of Enums
-// They cannot be created inside a class.
-
-enum TextSizes { small, medium, large }
-
-enum OrderStatus { processing, shipped, delivered }
-
-enum PaymentMethod { paypal, googlePay, applePay, visa, masterCard, creditCardd, paystack, razorPay, paytm }
--- lib/utils/constants/image_strings.dart
+++
@@ -1,9 +0,0 @@
-class TImages {
-  // -- App Logos
-  static const String darkAppLogo = '';
-  static const String lightAppLogo = '';
-
-  // -- Social Logos
-  static const String google = '';
-  static const String facebook = '';
-}
